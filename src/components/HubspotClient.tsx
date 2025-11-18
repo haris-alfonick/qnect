e07700927291cca6de385c0c69245cbf1cac7e18
mhmdharis <codeaccuracy@gmail.com>
2025-11-18 14:37:36 +0500

    dispatch Ravit products to cart

--- src/components/HubspotClient.tsx
+++ src/components/HubspotClient.tsx
@@ -1,21 +1,37 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useSearchParams, useRouter } from "next/navigation";
+import { addToCart, setCartOpen } from '@/lib/features/cart/cartSlice'
 import { useEffect } from "react";
+
 //import { useDispatch } from "react-redux";
 // import { addToCart } from "@/store/cartSlice";
 
 export default function HubspotAddPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  //const dispatch = useDispatch();
+  const dispatch = useAppDispatch()
+
+  const handleRevitLicense = (productId: string) => {
+    const item = {
+      id: productId === 'Free Trial' ? '1' : productId === 'express' ? '2' : '3',
+      name: 'Revit',
+      plan: productId,
+      quantity: 1,
+      price: productId === 'Free Trial' ? 0 : productId == 'express' ? 950 : 1900
+    };    
+
+    dispatch(addToCart(item))
+    router.push("/");
+    dispatch(setCartOpen(true))
+  }
 
   useEffect(() => {
     const productId = searchParams.get("productId");
-    const qty = Number(searchParams.get("qty")) || 1;
-
+    handleRevitLicense(productId!)
     if (productId) {
-      console.log(qty);
+      console.log(productId);
       // dispatch(addToCart({ id: productId, quantity: qty }));
       // router.push("/cart");
     } else {
